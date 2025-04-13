4338bf9d5ac09f4a17391a9b18037c981f89b74c
ArditMirena <mirenaardit@gmail.com>
2025-04-13 17:49:43 +0200

    Improved styling of Home page and added an contact form(not functional)

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,84 +1,69 @@
 import {
-    Anchor,
-    Button,
-    Checkbox,
-    Divider,
-    Group,
-    Paper,
-    PasswordInput,
-    Stack,
-    Text,
-    TextInput,
-  } from '@mantine/core';
-  
-  import { useState } from 'react';
+  Anchor,
+  Button,
+  Container,
+  Divider,
+  Group,
+  Paper,
+  PasswordInput,
+  Stack,
+  Text,
+  TextInput,
+} from '@mantine/core';
+import { PiGoogleLogoBold, PiFacebookLogoBold } from "react-icons/pi";
+import { Link } from 'react-router-dom';
 
 const Login = () => {
-    const [type, setType] = useState<'login' | 'register'>('login');
-
-    return (
-      <Paper shadow="xl" radius="md" p="xl">
+  return (
+    <Container size={420} my={40}>
+      <Paper shadow="xl" radius="md" p="xl" withBorder>
         <Text size="lg" fw={500}>
-          Welcome to <span style={{color: 'limegreen'}}>Stockly</span>, {type === 'login' ? 'login' : 'register'} with
+          Welcome back to <span><Link to={"/"} style={{textDecoration: 'none', color: 'limegreen'}}>Stockly</Link></span>
         </Text>
-  
+
         <Group grow mb="md" mt="md">
-          <Button variant="default" radius="xl">
+          <Button variant="default" radius="xl" disabled>
+            <PiGoogleLogoBold   size={20} />
+            &nbsp;
             Google
           </Button>
-          <Button variant="default" radius="xl">
-            Twitter
+          <Button variant="default" radius="xl" disabled>
+            <PiFacebookLogoBold   size={40} />
+            &nbsp;
+            Facebook
           </Button>
         </Group>
-  
+
         <Divider label="Or continue with email" labelPosition="center" my="lg" />
-  
-        <form>
+
+        <form style={{ textAlign: 'left' }}>
           <Stack>
-            {type === 'register' && (
-              <TextInput
-                label="Username"
-                placeholder="Your name"
-                radius="md"
-                required
-              />
-            )}
-  
             <TextInput
               required
               label="Email"
               placeholder="hello@stockly.com"
               radius="md"
             />
-  
             <PasswordInput
               required
               label="Password"
               placeholder="Your password"
               radius="md"
             />
-  
-            {type === 'register' && (
-              <Checkbox
-                label="I accept terms and conditions"
-                required
-              />
-            )}
           </Stack>
-  
+
           <Group justify="space-between" mt="xl">
-            <Anchor component="button" type="button" c="dimmed" size="xs" onClick={() => setType(type === 'register' ? 'login' : 'register')}>
-              {type === 'register'
-                ? 'Already have an account? Login'
-                : "Don't have an account? Register"}
+            <Anchor component={Link} to="/register" c="dimmed" size="xs">
+              Don’t have an account? Register
             </Anchor>
             <Button type="submit" radius="xl">
-              {type === 'register' ? 'Register' : 'Login'}
+              Login
             </Button>
           </Group>
         </form>
       </Paper>
-    );
-}
+    </Container>
+  );
+};
 
 export default Login;
--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -1,9 +1,74 @@
+import {
+  Anchor,
+  Button,
+  Checkbox,
+  Container,
+  Divider,
+  Group,
+  Paper,
+  PasswordInput,
+  Stack,
+  Text,
+  TextInput,
+} from '@mantine/core';
+import { PiGoogleLogoBold, PiFacebookLogoBold } from "react-icons/pi";
+import { Link } from 'react-router-dom';
+
 const Register = () => {
-    return (
-        <div>
-            <h1>Register</h1>
-        </div>
-    )
-}
+  return (
+    <Container size={420} my={40}>
+      <Paper shadow="xl" radius="md" p="xl" withBorder>
+        <Text size="lg" fw={500}>
+          Welcome back to <span><Link to={"/"} style={{ textDecoration: 'none', color: 'limegreen' }}>Stockly</Link></span>
+        </Text>
+
+        <Group grow mb="md" mt="md">
+          <Button variant="default" radius="xl" disabled>
+            <PiGoogleLogoBold size={20} />
+            &nbsp;
+            Google
+          </Button>
+          <Button variant="default" radius="xl" disabled>
+            <PiFacebookLogoBold size={40} />
+            &nbsp;
+            Facebook
+          </Button>
+        </Group>
+
+        <Divider label="Or register with email" labelPosition="center" my="lg" />
+
+        <form style={{ textAlign: 'left' }}>
+          <Stack>
+            <TextInput
+              label="Username"
+              placeholder="Your username"
+              radius="md"
+              required />
+            <TextInput
+              label="Email"
+              placeholder="hello@stockly.com"
+              radius="md"
+              required />
+            <PasswordInput
+              label="Password"
+              placeholder="Your password"
+              radius="md"
+              required />
+            <Checkbox label="I accept terms and conditions" required />
+          </Stack>
+
+          <Group justify="space-between" mt="xl">
+            <Anchor component={Link} to="/login" c="dimmed" size="xs">
+              Already have an account? Login
+            </Anchor>
+            <Button type="submit" radius="xl">
+              Register
+            </Button>
+          </Group>
+        </form>
+      </Paper>
+    </Container>
+  );
+};
 
 export default Register;
