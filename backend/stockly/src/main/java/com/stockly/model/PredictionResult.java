f58147318cd8df741f4d1985e3d664b471085549
BlendiGjoni <blendi.gjoni@outlook.com>
2025-05-09 16:36:12 +0200

    NoSQL connection and Prediction Model + Endpoints
    Made NoSQL Connection to MongoDB.
    Implemented Prediction Model, Service, Controller, to handle endpoints Get by Product/Warehouse, Get by Month, Get Current Month

---
+++ backend/stockly/src/main/java/com/stockly/model/PredictionResult.java
@@ -0,0 +1,94 @@
+package com.stockly.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+import java.time.Instant;
+
+@Getter
+@Setter
+public class PredictionResult {
+    @Id
+    private String id;
+
+    @Field("warehouse_id")
+    @JsonProperty("warehouse_id")
+    private Long warehouseId;
+
+    private Metadata metadata;
+
+    @Field("product_id")
+    @JsonProperty("product_id")
+    private Long productId;
+
+    @Field("data_hash")
+    @JsonProperty("data_hash")
+    private String dataHash;
+
+    @Field("stock_data")
+    @JsonProperty("stock_data")
+    private StockData stockData;
+
+    @Field("demand_forecast")
+    @JsonProperty("demand_forecast")
+    private DemandForecast demandForecast;
+
+    private Recommendation recommendation;
+}
+
+@Getter
+@Setter
+class Metadata {
+    @Field("created_at")
+    @JsonProperty("created_at")
+    private Instant createdAt;
+
+    private String version;
+    private String source;
+
+    @Field("prediction_run_id")
+    @JsonProperty("prediction_run_id")
+    private String predictionRunId;
+
+}
+
+@Getter
+@Setter
+class StockData {
+    private int current;
+
+    @Field("days_remaining")
+    @JsonProperty("days_remaining")
+    private double daysRemaining;
+}
+
+@Getter
+@Setter
+class DemandForecast {
+    @Field("daily_avg")
+    @JsonProperty("daily_avg")
+    private double dailyAvg;
+
+    @Field("daily_predicted")
+    @JsonProperty("daily_predicted")
+    private double dailyPredicted;
+
+    @Field("weekly_predicted_7d")
+    @JsonProperty("weekly_predicted_7d")
+    private double weeklyPredicted7d;
+}
+
+@Getter
+@Setter
+class Recommendation {
+    @Field("safety_stock")
+    @JsonProperty("safety_stock")
+    private int safetyStock;
+
+    @Field("suggested_restock")
+    @JsonProperty("suggested_restock")
+    private int suggestedRestock;
+}
